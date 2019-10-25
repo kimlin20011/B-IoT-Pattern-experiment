f65c1d3cebf75a0a56509260128288e0d07bdef2
Shun <kimlin20011@gmail.com>
2019-10-25 14:21:53 +0800

    depoly to edge and device

--- configs/config.js
+++ configs/config.js
@@ -25,7 +25,7 @@ module.exports ={
         bytecode: OFEI_QueryRegistry_Bytecode,
     },
     geth: {
-        account: `0xe8902cf406d7547fc3f69a2f463eb1463aa6b978`,
+        account: `0x5f187bda2c1d879596fa6714caf660b0d5ef426c`,
         //暫時不用
         password: process.env.password,
         gethWebsocketUrl:`ws://localhost:8546`,
